3b5de1372b258c1dde30ebed8f1e3a2361ebe5a4
lagdhir_parth <parthlagdhir2007@gmail.com>
2025-11-20 10:09:44 +0530

    Fisrt commit with Project setup of frontend and backend with express and DB connection

---
+++ server/db/connectDB.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MongoDB_URI}/${process.env.DB_NAME}`
+    );
+
+    console.log(
+      `MongoDB Connected !!! DB Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
