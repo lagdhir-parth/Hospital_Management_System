08b6262ad20420d76a3bf294f2c090f558b7f860
lagdhir_parth <parthlagdhir2007@gmail.com>
2025-11-23 16:07:12 +0530

    Implement department management features: add routes for retrieving all departments and department by ID; enhance create, update, and delete routes with JWT authentication.

--- server/src/controllers/department.controllers.js
+++ server/src/controllers/department.controllers.js
@@ -5,8 +5,21 @@ import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
 
 const createDepartment = asyncHandler(async (req, res) => {
-  const { name, description, headOfDepartmentUsername, location, contactNumber } =
-    req.body;
+  // if (req.role !== "admin") {
+  //   res.status(403);
+  //   throw new ApiError(
+  //     403,
+  //     "Forbidden: You don't have permission to perform create action"
+  //   );
+  // }
+
+  const {
+    name,
+    description,
+    headOfDepartmentUsername,
+    location,
+    contactNumber,
+  } = req.body;
 
   if ([name, description, location].some((field) => field?.trim() === "")) {
     res.status(400);
@@ -27,7 +40,7 @@ const createDepartment = asyncHandler(async (req, res) => {
       res.status(400);
       throw new ApiError(400, "Invalid head of department username");
     }
-    headOfDepartment = doctor._id;
+    headOfDepartment = doctor;
   }
 
   const newDepartment = await Department.create({
@@ -45,4 +58,119 @@ const createDepartment = asyncHandler(async (req, res) => {
     );
 });
 
-export { createDepartment };
+const getAllDepartments = asyncHandler(async (req, res) => {
+  const departments = await Department.find().populate(
+    "headOfDepartment",
+    "name username email" //only bring these three fields from the Doctor document
+  );
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, "Departments fetched successfully", departments)
+    );
+});
+
+const getDepartmentById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const department = await Department.findById(id).populate(
+    "headOfDepartment",
+    "name username email"
+  );
+
+  if (!department) {
+    res.status(404);
+    throw new ApiError(404, "Department not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Department fetched successfully", department));
+});
+
+const updateDepartmentById = asyncHandler(async (req, res) => {
+  // if (req.role !== "admin") {
+  //   res.status(403);
+  //   throw new ApiError(
+  //     403,
+  //     "Forbidden: You don't have permission to perform deletion action"
+  //   );
+  // }
+
+  const { id } = req.params;
+  const {
+    name,
+    description,
+    headOfDepartment,
+    contactNumber,
+    location,
+    status,
+  } = req.body;
+
+  if (name) {
+    const existingDepartment = await Department.findOne({ name });
+    if (existingDepartment && existingDepartment._id.toString() !== id) {
+      res.status(400);
+      throw new ApiError(400, "Department with this name already exists");
+    }
+  }
+
+  if (!["active", "inactive"].includes(status)) {
+    res.status(400);
+    throw new ApiError(400, "Invalid status value");
+  }
+
+  const department = await Department.findByIdAndUpdate(
+    id,
+    {
+      name,
+      description,
+      headOfDepartment,
+      contactNumber,
+      location,
+      status,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!department) {
+    res.status(404);
+    throw new ApiError(404, "Department not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Department updated successfully", department));
+});
+
+const deleteDepartmentById = asyncHandler(async (req, res) => {
+  // if (req.role !== "admin") {
+  //   res.status(403);
+  //   throw new ApiError(
+  //     403,
+  //     "Forbidden: You don't have permission to perform deletion action"
+  //   );
+  // }
+
+  const { id } = req.params;
+
+  const department = await Department.findByIdAndDelete(id);
+  if (!department) {
+    res.status(404);
+    throw new ApiError(404, "Department not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Department deleted successfully", department));
+});
+
+export {
+  createDepartment,
+  getAllDepartments,
+  getDepartmentById,
+  updateDepartmentById,
+  deleteDepartmentById,
+};
--- server/src/routes/department.routes.js
+++ server/src/routes/department.routes.js
@@ -1,9 +1,21 @@
-import {Router} from "express";
-import {createDepartment} from "../controllers/department.controllers.js";
+import { Router } from "express";
+import {
+  createDepartment,
+  getAllDepartments,
+  getDepartmentById,
+  updateDepartmentById,
+  deleteDepartmentById,
+} from "../controllers/department.controllers.js";
 import upload from "../middlewares/multer.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/create").post(upload.none(), createDepartment);
+router.route("/allDepartments").get(getAllDepartments);
+router.route("/find/:id").get(getDepartmentById);
 
+// Secure Routes:
+router.route("/create").post(verifyJWT, upload.none(), createDepartment);
+router.route("/delete/:id").delete(verifyJWT, deleteDepartmentById);
+router.route("/update/:id").put(verifyJWT, upload.none(), updateDepartmentById);
 export default router;
